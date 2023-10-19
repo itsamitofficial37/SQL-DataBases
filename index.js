5a75d7c6cc20b6ecf7367bdc8a9d0476936cefbd
itsamitofficial37 <amitofficial37@gmail.com>
2023-10-19 13:24:31 +0530

    third commit

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const path = require("path");
 const express = require("express");
 const methodOverride = require("method-override");
 
-
-
 const app = express();
 app.use(methodOverride("_method"));
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -49,7 +47,7 @@ app.get("/", (req, res) => {
   }
 });
 
-// user route 
+// user route
 
 app.get("/user", (req, res) => {
   try {
@@ -64,36 +62,99 @@ app.get("/user", (req, res) => {
   }
 });
 
-// edit routes 
+// edit routes
 
-app.get("/user/:id/edit",(req,res)=> {
-  let {id} = req.params;
+app.get("/user/:id/edit", (req, res) => {
+  let { id } = req.params;
   let query = `SELECT * FROM user WHERE id = '${id}'`;
   try {
-    connection.query(query,(err,result) => {
-      if(err) throw err;
-      let user =  result[0];
-    
-      res.render("edit.ejs",{user});
+    connection.query(query, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
 
+      res.render("edit.ejs", { user });
     });
-  }
-  catch(err) {
-    res.send(err)
+  } catch (err) {
+    res.send(err);
   }
 });
 
 // UPDATE ROUTES
 
-app.patch("user/:id",(req,res)=> {
-  res.send("Updated");
+app.patch("/user/:id", (req, res) => {
+  let { id } = req.params;
+  let { password: formPass, username: newUserName } = req.body;
+  let query = `SELECT * FROM user WHERE id = '${id}'`;
+  try {
+    connection.query(query, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+      if (formPass != user.password) {
+        res.send("Wrong Password Entered");
+      } else {
+        let query2 = `UPDATE user SET username='${newUserName}' WHERE id='${id}'`;
+        connection.query(query2, (err, result) => {
+          if (err) throw err;
+          res.send(result);
+        });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// DElete Routes
+
+app.get("/user/:id/delete", (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+  let q = `SELECT * FROM user WHERE id='${id}'`;
+  try {
+    connection.query(q, (err, user) => {
+      if (err) throw err;
+      
+      res.render("delete.ejs",{user});
+    });
+  } catch (err) {
+    res.send("Some error in Databases");
+  }
+});
+
+app.delete("/user/:id/", (req, res) => {
+  let { id } = req.params;
+  let { password } = req.body;
+  let q = `SELECT * FROM user WHERE id='${id}'`;
+
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+
+      if (user.password != password) {
+        res.send("WRONG Password entered!");
+      } else {
+        let q2 = `DELETE FROM user WHERE id='${id}'`; //Query to Delete
+        connection.query(q2, (err, result) => {
+          if (err) throw err;
+          else {
+            console.log(result);
+            console.log("deleted!");
+            res.redirect("/user");
+          }
+        });
+      }
+    });
+  } catch (err) {
+    res.send("some error with DB");
+  }
 });
 
 // app.get("/user/:id/edits", (req, res) => {
 //   let {id} = req.params;
 //   let query = `SELECT * FROM user WHERE id = '${id}'`;
 //   try {
-    
+
 //     connection.query(query, (err, result) => {
 //       if (err) throw err;
 //       console.log(result);
